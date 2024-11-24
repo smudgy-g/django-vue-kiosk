89504d31c34c0208f45c4216b32f1b4fb602e9ea
smudgy-g <adamgriff86@gmail.com>
2024-11-24 10:35:03 +0100

    add requirements

--- server/supplier/models.py
+++ server/supplier/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Supplier(models.Model):
+  id = models.UUIDField(auto_created=True, primary_key=True)
+  name = models.CharField(max_length=50)
+  
+
+  def __str__(self) -> str:
+    return self.name
